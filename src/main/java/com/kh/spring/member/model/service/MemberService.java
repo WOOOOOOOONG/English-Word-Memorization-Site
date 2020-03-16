40428dc8dbecc60579fcf8977148dd8ed603ba40
WOOOOOOOONG <sterme9@naver.com>
2020-03-16 21:26:26 +0900

    20200316 woong2

--- src/main/java/com/kh/spring/member/model/service/MemberService.java
+++ src/main/java/com/kh/spring/member/model/service/MemberService.java
@@ -54,8 +54,11 @@ public class MemberService {
 	public Member loginMember(Member m) {
 		
 		Member loginUser = mDao.selectMember(m);
+		if(loginUser != null && !bcryptPasswordEncoder.matches(m.getPwd(), loginUser.getPwd())) {
+			loginUser = null;
+		}else {
 			loginUser.setProfileimg(mDao.selectProfileImg(loginUser));
-		
+		}
 		return loginUser;
 	}
 
