860df0293614933a5810503a5f520f4b1543f678
user2 <user2@user2-PC>
2020-03-17 15:37:11 +0900

    0317 ssh

--- src/main/java/com/kh/spring/schedule/model/vo/Schedule.java
+++ src/main/java/com/kh/spring/schedule/model/vo/Schedule.java
@@ -8,7 +8,84 @@ public class Schedule {
 	private String sTitle; // 일정명
 	private Date startDate; // 시작일
 	private Date endDate; // 종료일 
-	private String vocaId; // 참조할 id
+	private String refId; // 참조할 id
+	private String refName; // 참조한 이름
+	private String color; // 일정 색깔
+	public Schedule() {
+	}
+	
+	public Schedule(int sId, String mId, String sTitle, Date startDate, Date endDate, String refId, String refName,
+			String color) {
+		super();
+		this.sId = sId;
+		this.mId = mId;
+		this.sTitle = sTitle;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.refId = refId;
+		this.refName = refName;
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getsId() {
+		return sId;
+	}
+	public void setsId(int sId) {
+		this.sId = sId;
+	}
+	public String getmId() {
+		return mId;
+	}
+	public void setmId(String mId) {
+		this.mId = mId;
+	}
+	public String getsTitle() {
+		return sTitle;
+	}
+	public void setsTitle(String sTitle) {
+		this.sTitle = sTitle;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public String getRefId() {
+		return refId;
+	}
+	public void setRefId(String refId) {
+		this.refId = refId;
+	}
+
+	public String getRefName() {
+		return refName;
+	}
+
+	public void setRefName(String refName) {
+		this.refName = refName;
+	}
+
+	@Override
+	public String toString() {
+		return "Schedule [sId=" + sId + ", mId=" + mId + ", sTitle=" + sTitle + ", startDate=" + startDate
+				+ ", endDate=" + endDate + ", refId=" + refId + ", refName=" + refName + ", color=" + color + "]";
+	}
+	
 	
 	
 	
