a4af22909abaa72a79da69f3a94b98afde737176
jae0014 <jaedafeeder@gmail.com>
2020-04-01 15:28:16 +0900

    Update FlashCardController.java

--- src/main/java/com/kh/spring/flashcard/controller/FlashCardController.java
+++ src/main/java/com/kh/spring/flashcard/controller/FlashCardController.java
@@ -5,12 +5,13 @@ import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.spring.member.model.vo.Member;
@@ -19,7 +20,6 @@ import com.kh.spring.schedule.model.vo.Schedule;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
-@SessionAttributes("whatclick")
 @Controller
 public class FlashCardController {
 
@@ -38,10 +38,38 @@ public class FlashCardController {
 		//Member mem = (Member)session.getAttribute("loginMember");
 		//System.out.println(mem);
 		//mv.addObject("loginMember",mem);
-		mv.addObject("whatclick", "dan");
 		mv.setViewName("flashcard/maincardset");	
 		return mv; 
 	}
+	@RequestMapping("flashcard_other_RO.fl")
+	public ModelAndView oneCardView(ModelAndView mv,
+			@RequestParam(value="csid", required=true) String CSID,
+			@RequestParam(value="title", required=true) String title,
+			@RequestParam(value="edit", required=false) boolean edit,
+			HttpSession session ) {
+		//Member mem = (Member)session.getAttribute("loginMember");
+		//System.out.println(mem);
+		//mv.addObject("loginMember",mem);
+		mv.addObject("csid", CSID);
+		mv.addObject("title", title);
+		mv.addObject("edit", edit);
+		mv.setViewName("flashcard/just_view");	
+		return mv; 
+	}
 	
+	@RequestMapping("flashcard_edit.fl")
+	public ModelAndView oneEditView(ModelAndView mv,
+			@RequestParam(value="csid", required=true) String CSID,
+			@RequestParam(value="title", required=true) String title,
+		
+			HttpSession session ) {
+		//Member mem = (Member)session.getAttribute("loginMember");
+		//System.out.println(mem);
+		//mv.addObject("loginMember",mem);
+		mv.addObject("csid", CSID);
+		mv.addObject("title", title);
 	
+		mv.setViewName("flashcard/just_readview");	
+		return mv; 
+	}
 }
